25553295668dde562275b0db67275e4d1400c3b6
TakaroYont <takaroyontt@outlook.com>
2023-02-16 16:53:28 +0800

    Feature: 2.在历史记录列表中加粗显示当前选择的项目。

--- src/index.js
+++ src/index.js
@@ -90,7 +90,10 @@ class Game extends React.Component {
             const desc = move ? 'Go to move #' + move + ' ' + coordinate : 'Go to game start';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button className="jump-to"
+                            onClick={() => this.jumpTo(move)}>
+                        {desc}
+                    </button>
                 </li>
             );
         })
